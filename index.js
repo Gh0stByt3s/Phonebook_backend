7d4fe7d0c844a36e6fab2cb88e95ece98dd8908e
Gh0stByt3s <orangageorge.gr@gmail.com>
2023-05-06 14:58:01 +0300

    Delete and update operations done

--- index.js
+++ index.js
@@ -1,64 +1,47 @@
-require("dotenv").config();
-const { log } = require("console");
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/persons");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const Person = require("./models/persons");
+const morgan = require("morgan");
 
 // morgan.token("body", function (req) {
 //   console.log(req.body);
 //   return JSON.stringify(req.body);
 // });
 
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-// app.use(morgan(":method :url :response-time ms :body"));
-app.use(cors());
 app.use(express.static("dist"));
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/info", (request, response) => {
-  response.send(`<div>
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>
-  </div>`);
-});
+// app.use(morgan(":method :url :response-time ms :body"));
 
 // Fetching all the persons
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
+  Person.find().then((persons) => {
     response.json(persons);
   });
 });
-
 // Fetching a single person
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      person ? response.json(person) : response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
 // Creating a new person
@@ -67,33 +50,41 @@ app.post("/api/persons", (request, response) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.random() * 50,
   });
-  const personName = persons.map((person) => person.name.toLowerCase());
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ error: "name or number missing" });
-  }
-
-  if (personName.includes(body.name.toLowerCase())) {
-    return response
-      .status(400)
-      .json({ error: "name already exists in the phonebook" });
-  }
-
-  persons = persons.concat(person);
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
 });
 
-// Deleting a single person
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+// Editing a person
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-  response.status(204).end();
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+// Deleting a single person
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// Middleware handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+// Middleware handler with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
